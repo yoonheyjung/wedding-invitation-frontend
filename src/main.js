75eef887ee328d1f71f41937854876f40c9e3f47
혜정 <jung5318@gmail.com>
2023-02-05 22:45:58 +0900

    클립보드 기능

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import Countdown from "vue3-flip-countdown";
+import VueClipboard from "vue-clipboard2";
 
 // font-awesome과 관련된 import를 정의
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 library.add(faUserSecret);
+VueClipboard.config.autoSetContainer = true;
+// Vue.use(VueClipboard)
 
 const app = createApp(App);
 // 위에 createApp을 통해 생성한 Vue Application 인스턴스의 component API 활용
 app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.use(Countdown);
+app.use(VueClipboard);
 app.mount("#app");
