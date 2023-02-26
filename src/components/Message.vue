288d7f8ec4606a36f49790232430e41f6f8adfaf
혜정 <jung5318@gmail.com>
2023-02-27 04:51:30 +0900

    css add

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -6,18 +6,18 @@
       방명록은 2월 26일에 열립니다. 조금만 기다려주세요 ❤️
     </h1> -->
     <div class="flex mx-5" :items="comments">
-      <div class="flex-shrink-0 mr-3">
+      <!-- <div class="flex-shrink-0 mr-3">
         <img
           class="mt-2 rounded-full w-8 h-8 sm:w-10 sm:h-10"
           src="https://images.unsplash.com/photo-1604426633861-11b2faead63c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=200&q=80"
           alt=""
         />
-      </div>
+      </div> -->
       <div
-        class="flex-1 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed"
+        class="flex-1 my-3 border rounded-lg px-4 py-2 sm:px-6 sm:py-4 leading-relaxed"
       >
         <strong>Sarah</strong>
-        <span class="text-xs text-gray-400">3:34 PM</span>
+        <span class="text-xs text-gray-400 mx-2">3:34 PM</span>
         <p class="text-sm">
           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
           nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
